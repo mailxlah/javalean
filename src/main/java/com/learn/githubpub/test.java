061aa150841dcb92f1d730a569b46ef947ca4e17
mailxlah <mailxl@163.com>
2018-05-23 19:45:51 +0800

    Update test.java

--- src/main/java/com/learn/githubpub/test.java
+++ src/main/java/com/learn/githubpub/test.java
@@ -1,14 +1,4 @@
-/**
- * Copyright (C), 2017-2018, 北京畅游天下网络技术有限公司
- *
- * @PROJECT_NAME: 魔盒系统
- * @FileName: test
- * @author: xulei4_m
- * @Date: 2018/5/23 18:29
- * @Description:
- * @History:
- * @ <author>          <time>          <version>          <desc>
- */
+
 package com.learn.githubpub;
 
 public class test {
